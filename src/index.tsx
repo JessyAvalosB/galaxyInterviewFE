a64a6c9f2f94648e7924dee0faeaa0b7e6f161bf
Jessy Avalos Barrales <avalosbarralesjessy@gmail.com>
2025-02-13 19:56:13 -0600

    mui implemented

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root');
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { DefaultTheme as theme } from './themes';
+
+const container = document.getElementById('root') as HTMLElement;
 
 if (container) {
     const root = createRoot(container);
-    root.render(<App />);
+    root.render(
+        <React.StrictMode>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+        </React.StrictMode>);
 }
